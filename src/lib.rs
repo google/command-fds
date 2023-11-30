dc679551047756cd6c43b386569a71cb5a223423
Andrew Walbran <qwandor@google.com>
2023-11-30 16:12:12 +0000

    Explain issue with calling `fd_mappings` more than once.

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,9 @@ pub trait CommandFdExt {
     /// Adds the given set of file descriptors to the command.
     ///
     /// Warning: Calling this more than once on the same command may result in unexpected behaviour.
+    /// In particular, it is not possible to check that two mappings applied separately don't use
+    /// the same `child_fd`. If there is such a collision then one will apply and the other will be
+    /// lost.
     fn fd_mappings(&mut self, mappings: Vec<FdMapping>) -> Result<&mut Self, FdMappingCollision>;
 
     /// Adds the given set of file descriptors to be passed on to the child process when the command
