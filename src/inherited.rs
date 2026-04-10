e56960526e3fb5a41db3befdad271b849d052724
Andrew Walbran <qwandor@google.com>
2026-04-10 14:40:20 +0100

    Clean up and fix for new version of nix.

--- src/inherited.rs
+++ src/inherited.rs
@@ -12,25 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Library for safely obtaining `OwnedFd` for inherited file descriptors.
+//! Utilities for safely obtaining `OwnedFd`s for inherited file descriptors.
 
-use nix::fcntl::{F_SETFD, FdFlag, fcntl};
-use nix::libc;
-use std::collections::HashMap;
-use std::fs::canonicalize;
-use std::fs::read_dir;
-use std::os::fd::FromRawFd;
-use std::os::fd::OwnedFd;
-use std::os::fd::RawFd;
-use std::sync::Mutex;
-use std::sync::OnceLock;
+use nix::{
+    fcntl::{F_SETFD, FdFlag, fcntl},
+    libc,
+};
+use std::{
+    collections::HashMap,
+    fs::{canonicalize, read_dir},
+    os::fd::{FromRawFd, OwnedFd, RawFd},
+    sync::{Mutex, OnceLock},
+};
 use thiserror::Error;
+
+static INHERITED_FDS: OnceLock<Mutex<HashMap<RawFd, Option<OwnedFd>>>> = OnceLock::new();
 
 /// Errors that can occur while taking an ownership of `RawFd`
 #[derive(Debug, PartialEq, Error)]
-pub enum Error {
-    /// init_once() not called
-    #[error("init_once() not called")]
+pub enum InheritedFdError {
+    /// init_inherited_fds() not called
+    #[error("init_inherited_fds() not called")]
     NotInitialized,
 
     /// Ownership already taken
@@ -42,15 +44,16 @@ pub enum Error {
     FileDescriptorNotInherited(RawFd),
 }
 
-static INHERITED_FDS: OnceLock<Mutex<HashMap<RawFd, Option<OwnedFd>>>> = OnceLock::new();
-
-/// Take ownership of all open file descriptors in this process, which later can be obtained by
-/// calling `take_fd_ownership`. Set the FD_CLOEXEC on all of these file descriptors.
+/// Takes ownership of all open file descriptors in this process other than standard
+/// input/output/error, so that they can later be obtained by calling [`take_fd_ownership`].
+///
+/// Sets the `FD_CLOEXEC` flag on all of these file descriptors.
 ///
 /// # Safety
-/// This function has to be called very early in the program before the ownership of any file
-/// descriptors (except stdin/out/err) is taken.
-pub unsafe fn init_once() -> Result<(), std::io::Error> {
+///
+/// This must be called very early in the program, before the ownership of any file descriptors
+/// (except stdin/out/err) is taken.
+pub unsafe fn init_inherited_fds() -> Result<(), std::io::Error> {
     let mut fds = HashMap::new();
 
     let fd_path = canonicalize("/proc/self/fd")?;
@@ -74,13 +77,13 @@ pub unsafe fn init_once() -> Result<(), std::io::Error> {
             continue;
         }
 
-        fcntl(raw_fd, F_SETFD(FdFlag::FD_CLOEXEC))?;
-
-        // SAFETY: /proc/self/fd/* are file descriptors that are open. If `init_once()` was called
-        // at the very beginning of the program execution (as requested by the safety requirement
-        // of this function), this is the first time to claim the ownership of these file
-        // descriptors.
+        // SAFETY: /proc/self/fd/* are file descriptors that are open. If `init_inherited_fds()` was
+        // called at the very beginning of the program execution (as requested by the safety
+        // requirement of this function), this is the first time to claim the ownership of these
+        // file descriptors.
         let owned_fd = unsafe { OwnedFd::from_raw_fd(raw_fd) };
+
+        fcntl(&owned_fd, F_SETFD(FdFlag::FD_CLOEXEC))?;
         fds.insert(raw_fd, Some(owned_fd));
     }
 
@@ -91,13 +94,16 @@ pub unsafe fn init_once() -> Result<(), std::io::Error> {
         )))
 }
 
-/// Take the ownership of the given `RawFd` and returns `OwnedFd` for it. The returned FD is set
-/// CLOEXEC. `Error` is returned when the ownership was already taken (by a prior call to this
+/// Takes the ownership of the given `RawFd` and returns an `OwnedFd` for it.
+///
+/// The returned FD will have the `FD_CLOEXEC` flag set.
+///
+/// An error is returned when the ownership was already taken (by a prior call to this
 /// function with the same `RawFd`) or `RawFd` is not an inherited file descriptor.
-pub fn take_fd_ownership(raw_fd: RawFd) -> Result<OwnedFd, Error> {
+pub fn take_fd_ownership(raw_fd: RawFd) -> Result<OwnedFd, InheritedFdError> {
     let mut fds = INHERITED_FDS
         .get()
-        .ok_or(Error::NotInitialized)?
+        .ok_or(InheritedFdError::NotInitialized)?
         .lock()
         .unwrap();
 
@@ -105,20 +111,21 @@ pub fn take_fd_ownership(raw_fd: RawFd) -> Result<OwnedFd, Error> {
         if let Some(owned_fd) = value.take() {
             Ok(owned_fd)
         } else {
-            Err(Error::OwnershipTaken(raw_fd))
+            Err(InheritedFdError::OwnershipTaken(raw_fd))
         }
     } else {
-        Err(Error::FileDescriptorNotInherited(raw_fd))
+        Err(InheritedFdError::FileDescriptorNotInherited(raw_fd))
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use anyhow::Result;
-    use nix::fcntl::{F_GETFD, F_SETFD, FdFlag, fcntl};
     use nix::unistd::close;
-    use std::os::fd::{AsRawFd, IntoRawFd};
+    use std::{
+        io,
+        os::fd::{AsRawFd, IntoRawFd},
+    };
     use tempfile::tempfile;
 
     struct Fixture {
@@ -126,7 +133,7 @@ mod test {
     }
 
     impl Fixture {
-        fn setup(num_fds: usize) -> Result<Self> {
+        fn setup(num_fds: usize) -> Result<Self, io::Error> {
             let mut fds = Vec::new();
             for _ in 0..num_fds {
                 fds.push(tempfile()?.into_raw_fd());
@@ -134,7 +141,7 @@ mod test {
             Ok(Fixture { fds })
         }
 
-        fn open_new_file(&mut self) -> Result<RawFd> {
+        fn open_new_file(&mut self) -> Result<RawFd, io::Error> {
             let raw_fd = tempfile()?.into_raw_fd();
             self.fds.push(raw_fd);
             Ok(raw_fd)
@@ -150,22 +157,22 @@ mod test {
     }
 
     fn is_fd_opened(raw_fd: RawFd) -> bool {
-        fcntl(raw_fd, F_GETFD).is_ok()
+        unsafe { libc::fcntl(raw_fd, libc::F_GETFD) != -1 }
     }
 
     #[test]
-    fn happy_case() -> Result<()> {
-        let fixture = Fixture::setup(2)?;
+    fn happy_case() {
+        let fixture = Fixture::setup(2).unwrap();
         let f0 = fixture.fds[0];
         let f1 = fixture.fds[1];
 
         // SAFETY: assume files opened by Fixture are inherited ones
         unsafe {
-            init_once()?;
+            init_inherited_fds().unwrap();
         }
 
-        let f0_owned = take_fd_ownership(f0)?;
-        let f1_owned = take_fd_ownership(f1)?;
+        let f0_owned = take_fd_ownership(f0).unwrap();
+        let f1_owned = take_fd_ownership(f1).unwrap();
         assert_eq!(f0, f0_owned.as_raw_fd());
         assert_eq!(f1, f1_owned.as_raw_fd());
 
@@ -173,104 +180,108 @@ mod test {
         drop(f1_owned);
         assert!(!is_fd_opened(f0));
         assert!(!is_fd_opened(f1));
-        Ok(())
     }
 
     #[test]
-    fn access_non_inherited_fd() -> Result<()> {
-        let mut fixture = Fixture::setup(2)?;
+    fn access_non_inherited_fd() {
+        let mut fixture = Fixture::setup(2).unwrap();
 
         // SAFETY: assume files opened by Fixture are inherited ones
         unsafe {
-            init_once()?;
+            init_inherited_fds().unwrap();
         }
 
-        let f = fixture.open_new_file()?;
+        let f = fixture.open_new_file().unwrap();
         assert_eq!(
-            Some(Error::FileDescriptorNotInherited(f)),
-            take_fd_ownership(f).err()
+            take_fd_ownership(f).err(),
+            Some(InheritedFdError::FileDescriptorNotInherited(f))
         );
-        Ok(())
     }
 
     #[test]
-    fn call_init_once_multiple_times() -> Result<()> {
-        let _ = Fixture::setup(2)?;
+    fn call_init_inherited_fds_multiple_times() {
+        let _ = Fixture::setup(2).unwrap();
 
         // SAFETY: assume files opened by Fixture are inherited ones
         unsafe {
-            init_once()?;
+            init_inherited_fds().unwrap();
         }
 
         // SAFETY: for testing
-        let res = unsafe { init_once() };
+        let res = unsafe { init_inherited_fds() };
         assert!(res.is_err());
-        Ok(())
     }
 
     #[test]
-    fn access_without_init_once() -> Result<()> {
-        let fixture = Fixture::setup(2)?;
+    fn access_without_init_inherited_fds() {
+        let fixture = Fixture::setup(2).unwrap();
 
         let f = fixture.fds[0];
-        assert_eq!(Some(Error::NotInitialized), take_fd_ownership(f).err());
-        Ok(())
+        assert_eq!(
+            take_fd_ownership(f).err(),
+            Some(InheritedFdError::NotInitialized)
+        );
     }
 
     #[test]
-    fn double_ownership() -> Result<()> {
-        let fixture = Fixture::setup(2)?;
+    fn double_ownership() {
+        let fixture = Fixture::setup(2).unwrap();
         let f = fixture.fds[0];
 
         // SAFETY: assume files opened by Fixture are inherited ones
         unsafe {
-            init_once()?;
+            init_inherited_fds().unwrap();
         }
 
-        let f_owned = take_fd_ownership(f)?;
+        let f_owned = take_fd_ownership(f).unwrap();
         let f_double_owned = take_fd_ownership(f);
-        assert_eq!(Some(Error::OwnershipTaken(f)), f_double_owned.err());
+        assert_eq!(
+            f_double_owned.err(),
+            Some(InheritedFdError::OwnershipTaken(f)),
+        );
 
         // just to highlight that f_owned is kept alive when the second call to take_fd_ownership
         // is made.
         drop(f_owned);
-        Ok(())
     }
 
     #[test]
-    fn take_drop_retake() -> Result<()> {
-        let fixture = Fixture::setup(2)?;
+    fn take_drop_retake() {
+        let fixture = Fixture::setup(2).unwrap();
         let f = fixture.fds[0];
 
         // SAFETY: assume files opened by Fixture are inherited ones
         unsafe {
-            init_once()?;
+            init_inherited_fds().unwrap();
         }
 
-        let f_owned = take_fd_ownership(f)?;
+        let f_owned = take_fd_ownership(f).unwrap();
         drop(f_owned);
 
         let f_double_owned = take_fd_ownership(f);
-        assert_eq!(Some(Error::OwnershipTaken(f)), f_double_owned.err());
-        Ok(())
+        assert_eq!(
+            f_double_owned.err(),
+            Some(InheritedFdError::OwnershipTaken(f)),
+        );
     }
 
     #[test]
-    fn cloexec() -> Result<()> {
-        let fixture = Fixture::setup(2)?;
+    fn cloexec() {
+        let fixture = Fixture::setup(2).unwrap();
         let f = fixture.fds[0];
 
-        fcntl(f, F_SETFD(FdFlag::empty()))?;
+        let res = unsafe { libc::fcntl(f.as_raw_fd(), libc::F_SETFD, 0) };
+        assert_ne!(res, -1);
 
         // SAFETY: assume files opened by Fixture are inherited ones
         unsafe {
-            init_once()?;
+            init_inherited_fds().unwrap();
         }
 
-        // FD_CLOEXEC should be set by init_once
-        let flags = fcntl(f.as_raw_fd(), F_GETFD)?;
+        // SAFETY: F_GETFD doesn't need any extra parameters.
+        let flags = unsafe { libc::fcntl(f.as_raw_fd(), libc::F_GETFD) };
+        assert_ne!(flags, -1);
+        // FD_CLOEXEC should be set by init_inherited_fds
         assert_eq!(flags, FdFlag::FD_CLOEXEC.bits());
-
-        Ok(())
     }
 }
